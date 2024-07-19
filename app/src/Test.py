fab6360d98950a173069a855a2604e06a12ba849
Sri Charan Bodduna <68746674+sricharan245@users.noreply.github.com>
2024-07-19 14:13:27 -0500

    [case 2] using PromptTemplate & chaining

--- app/src/Test.py
+++ app/src/Test.py
@@ -5,9 +5,11 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_community.llms import OCIGenAI
 
 from oci.config import from_file
+
+# Step 1 - authenticate using "DEFAULT" profile
 config = from_file(file_location="~/.oci/config")
 
-# use default authN method API-key
+# Step 2 - Setup OCI Generative AI llm
 llm = OCIGenAI(
     # model_id="ocid1.generativeaimodel.oc1.us-chicago-1.amaaaaaask7dceyawk6mgunzodenakhkuwxanvt6wo3jcpf72ln52dymk4wq",
     model_id = "cohere.command",
@@ -19,3 +21,31 @@ llm = OCIGenAI(
 response = llm.invoke("Tell me one fact about space", temperature=0.7)
 print("Case 1 Response:", response)
 
+#############
+# Step 3 - use string prompt to accept text input. Here we create a template and declare a input variable {human_input}
+
+# String prompt
+
+template = """You are a chatbot having a conversation with a human.
+Human: {human_input} + {city}
+"""
+
+# step 4 - Here we create a Prompt using the template
+
+prompt2 = PromptTemplate(input_variables=["human_input", "city"], template=template)
+
+prompt_val = prompt2.invoke({"human_input":"Tell us in a exciting tone about", "city": "Las Vegas"})
+print ("Prompt String is -> ")
+print (prompt_val.to_string())
+
+#Step 5 - here we declare a chain that beigns with a prompt, next llm and finally output parser
+
+chain = prompt2 | llm
+
+# Step 6 - Next we invoke a chain and provide input question
+
+response = chain.invoke({"human_input":"Tell us in a exciting tone about", "city": "Las Vegas"})
+
+#Step 7 - print the prompt and response from the llm
+
+print("Case2 Response - > ", response)
